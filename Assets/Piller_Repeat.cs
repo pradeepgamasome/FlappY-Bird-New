bfef7951084b2586a0c3007ce7b2edee1c5b386e
pradeepgamasome <jack@Mohankumars-Mac-mini.local>
2016-11-02 12:28:31 +0530

    Perfect Flappy Bird
    Done on 1st of nov. 2016

---
+++ Assets/Piller_Repeat.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Piller_Repeat : MonoBehaviour {
+	
+	float f;
+	float speed=1.5f;
+	public GameObject g;
+
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 1; i < 19; i++) 
+		{
+			Instantiate (g,new  Vector3 ((5*i)+2, 1+Random.Range (-2, 2), 0), Quaternion.identity);
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+//		speed = 1.5f;
+		transform.position = new Vector3 (this.transform.position.x-speed*Time.deltaTime,this.transform.position.y,0);
+
+		if(transform.position.x<-12)
+		{
+			transform.position = new Vector3 (55,Random.Range (-4, 4),0);
+		}
+
+	}
+}
---
+++ Assets/Play.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+
+public class Play : MonoBehaviour {
+	float y=0;
+	float speed;
+	float acc;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		speed = 1 + Time.deltaTime;
+		acc = speed*0.5f;
+
+
+	}
+
+	float position_y;
+
+	float speed_y = 0;
+
+	float accel_y=2;
+
+	// Update is called once per frame
+	void Update () {
+
+
+
+
+	
+
+		speed_y += accel_y*Time.deltaTime;
+		position_y -= speed_y;
+
+		transform.position = new Vector3 (0,position_y,0);
+
+		if ( Input.GetKeyDown(KeyCode.UpArrow) )
+
+		{
+			
+			speed_y =-0.5f;
+
+		}
+
+	}
+}
